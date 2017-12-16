65830bec8e142ff97c907bb6d11019c0a2c9237c
Othman Bichouna <obichouna@stuy.edu>
2017-12-15 23:31:34 -0500

    Working on it

--- basic_server.c
+++ basic_server.c
@@ -1,16 +1,25 @@
 #include "pipe_networking.h"
 
 
+void process(char * s){
+  //Simple process that makes all the string uppercase
+  int x = 0;
+  while(x<strlen(s)){
+    s[x++] -= 32;
+  }
+
+}
 int main() {
 
   int to_client;
   int from_client;
   char buffer[256];
 
-  from_client = server_handshake( &to_client );
-  while(read(from_client, buffer, sizeof(buffer))){
-    printf("Message recieved: %s", buffer); 
-    write(to_client, buffer, sizeof(buffer));
+  while(1){
+    from_client = server_handshake( &to_client );
+    while(read(from_client, buffer, sizeof(buffer))){
+      printf("Message recieved: %s", buffer);
+      write(to_client, buffer, sizeof(buffer));
+    }
   }
 }
-
